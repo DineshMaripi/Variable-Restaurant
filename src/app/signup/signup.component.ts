134837c0f22b1ddea299a9c4c3e8c10e89807ccc
DineshMaripi <dineshmaripi.ui@gmail.com>
2023-09-04 16:51:54 +0530

    done

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -4,48 +4,53 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent {
-
-  signUpForm!:FormGroup;
+  signUpForm!: FormGroup;
   constructor(private formBuilder: FormBuilder) {
-    this.signUpForm=this.formBuilder.group({
-      firstName:['',[Validators.required]],
-      lastName:['',[Validators.required]],
-      email:['', [Validators.required, Validators.email]],
-      password:[ '',
-      [
-        Validators.required,
-        Validators.minLength(5),
-        Validators.maxLength(10),
-      ]
-    ],
-      conformpassword:[ '',
-      [
-        Validators.required,
-        Validators.minLength(5),
-        Validators.maxLength(10),
-      ]
-    ],
-      checkbox:[]
-
-    },{validator:this.passwordMatchValidator })
+    this.signUpForm = this.formBuilder.group(
+      {
+        firstName: ['', [Validators.required]],
+        lastName: ['', [Validators.required]],
+        email: ['', [Validators.required, Validators.email]],
+        password: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(5),
+            Validators.maxLength(10),
+          ],
+        ],
+        conformpassword: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(5),
+            Validators.maxLength(10),
+          ],
+        ],
+        checkbox: [],
+      },
+      { validator: this.passwordMatchValidator }
+    );
   }
 
-  passwordMatchValidator = (control: FormGroup): { [key: string]: boolean } | null => {
+  passwordMatchValidator = (
+    control: FormGroup
+  ): { [key: string]: boolean } | null => {
     const password = control.get('password')?.value;
     const confirmPassword = control.get('conformpassword')?.value;
-  
+
     if (password !== confirmPassword) {
-      return { 'passwordMismatch': true };
+      return { passwordMismatch: true };
     }
-  
+
     return null;
   };
-  onSubmit(){
-  if(this.signUpForm){
-    console.log(this.signUpForm.value)
+  onSubmit() {
+    if (this.signUpForm) {
+      console.log(this.signUpForm.value);
+    }
   }
- }
 }
