048ce997ae02ec46766524cac6339b85fd926adc
DineshMaripi <dineshmaripi.ui@gmail.com>
2023-09-20 13:23:37 +0530

    changes Done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,17 +4,18 @@ import { LoginComponent } from './login/login.component';
 import { FoodItemComponent } from './Fooditems/fooditem.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { SignupComponent } from './signup/signup.component';
-import { ContactComponent } from './contact/contact.component';
-import { BiryaniComponent } from './foodmenu/biryani/biryani/biryani.component';
+import { CartComponent } from './cart/cart.component';
+
 
 const routes: Routes = [
   { path: '', component: FoodItemComponent},
   { path: 'home', component: HomepageComponent},
   { path: 'login', component: LoginComponent },
-  { path: 'contact', component: ContactComponent},
+  { path: 'cart', component: CartComponent},
   { path: 'signup', component: SignupComponent},
-  {path:'biryanis' ,component:BiryaniComponent}
-  // other routes
+  { path: 'foodmenu', loadChildren: () => import('./foodmenu/foodmenu.module').then(m => m.FoodmenuModule) },
+  
+
 ];
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,15 @@ import { NavBarComponent } from './Navbar/navbar.component';
 import { SideBarComponent } from './SideBar/sidebar.component';
 import { FoodItemComponent } from './Fooditems/fooditem.component';
 import { FoodItemService } from './Services/fooditem.service';
-import { RouterModule, Routes } from '@angular/router';
 import { FoodmenuModule } from './foodmenu/foodmenu.module';
-import { BiryaniComponent } from './foodmenu/biryani/biryani/biryani.component';
 import { LoginComponent } from './login/login.component';
-import { ContactComponent } from './contact/contact.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { SignupComponent } from './signup/signup.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
-
-
+import { ImageSliderComponent } from './image-slider/image-slider.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgImageSliderModule } from 'ng-image-slider';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
@@ -26,12 +24,22 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     SideBarComponent,
     FoodItemComponent,
     LoginComponent,
-    ContactComponent,
     HomepageComponent,
     SignupComponent,
+    ImageSliderComponent,
+    CartComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule,ReactiveFormsModule,FoodmenuModule,FormsModule],
- 
+  imports: [
+    NgImageSliderModule,
+    BrowserAnimationsModule,
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FoodmenuModule,
+    FormsModule,
+  ],
+
   providers: [FoodItemService],
   bootstrap: [AppComponent],
 })
